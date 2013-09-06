7794bb933097ef91fb7b30219d984d23474e24e8
marcomorawec <marco.morawec@gmail.com>
2013-09-06 10:56:26 -0400

    adding the controller and the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ WeekendApp::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  resources :pins 
+
+  root :to => 'pins#index'
+
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -54,5 +59,5 @@ WeekendApp::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  match ':controller(/:action(/:id))(.:format)'
 end
