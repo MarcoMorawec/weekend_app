0cede93d00ecfbb23ed3b271b4e04e41ea7b9046
marcomorawec <marco.morawec@gmail.com>
2013-09-06 10:08:56 -0400

    pushing the rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeekendApp::Application.config.secret_token = '0f5c054be952222c8f33533261fd417f435a0ec04752db16824077afcf6e37af6dded5ce1529df5ce52c3d8d7f5d1e07f179adac6bb3c328b386586d1bb90461'
